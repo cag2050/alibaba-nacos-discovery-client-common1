476887de412e18b82478672d17f16089456812e4
chenanguo <cag2050@163.com>
2019-11-26 17:59:01 +0800

    project init

---
+++ src/main/java/com/example/alibabanacosdiscoveryclientcommon1/AlibabaNacosDiscoveryClientCommon1Application.java
@@ -0,0 +1,13 @@
+package com.example.alibabanacosdiscoveryclientcommon1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaNacosDiscoveryClientCommon1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaNacosDiscoveryClientCommon1Application.class, args);
+    }
+
+}

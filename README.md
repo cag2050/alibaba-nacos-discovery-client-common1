fdbe1694318891ae286be7597a981f43ce519ec6
chenanguo <cag2050@163.com>
2019-11-26 19:13:07 +0800

    add: 依赖,属性,controller

---
+++ README.md
@@ -0,0 +1,16 @@
+### Nacos 服务消费者
+
+### 注意：
+1.本项目版本：
+
+Spring Cloud Version| Spring Cloud Alibaba Version | Spring Boot Version
+--- | --- | ---
+Spring Cloud Greenwich | 2.1.1.RELEASE | 2.1.X.RELEASE
+
+### 参考资料
+
+参考资料 | 网址
+--- | ---
+Spring Cloud Alibaba基础教程：使用Nacos实现服务注册与发现 | http://blog.didispace.com/spring-cloud-alibaba-1/
+使用 Nacos 的 Docker 镜像，启动 Nacos 服务 | https://www.cnblogs.com/cag2050/p/11918293.html
+组件版本关系 | https://github.com/alibaba/spring-cloud-alibaba/wiki/%E7%89%88%E6%9C%AC%E8%AF%B4%E6%98%8E
---
+++ src/main/java/com/example/alibabanacosdiscoveryclientcommon1/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.example.alibabanacosdiscoveryclientcommon1.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class TestController {
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+
+    @RequestMapping("/test")
+    public String test() {
+        // 通过spring cloud common中的负载均衡接口选取服务提供节点实现接口调用
+        ServiceInstance serviceInstance = loadBalancerClient.choose("alibaba-nacos-discovery-server");
+        String url = serviceInstance.getUri() + "/hello?name=" + "didi";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(url, String.class);
+        return "Invoke : " + url + ", return : " + result;
+    }
+}
